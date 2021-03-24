bbc812f62a08a50f589d7c6391c4c91aad2675da
Menno <my.bil@student.avans.nl>
2021-03-24 13:59:24 +0100

    [FINISHED] servo control

--- Eindopdracht/Eindopdracht/main.c
+++ Eindopdracht/Eindopdracht/main.c
@@ -9,9 +9,9 @@
 
 void wait(int ms);
 
-// Extern variable to the servo1 angle, value between 250-1500 (defined in servo.h)
+// Extern variable to the servo1 angle (defined in servo.h)
 int servo1_targetValue;
-// Extern variable to the servo2 angle, value between 250-2500 (defined in servo.h)
+// Extern variable to the servo2 angle (defined in servo.h)
 int servo2_targetValue;
 
 
@@ -22,12 +22,15 @@ int main( void )
 	
 	while (1)
 	{		
-		servo1_targetValue = 1500;
-		servo2_targetValue = 250;
-		wait(10000);
-		servo1_targetValue = 250;
-		servo2_targetValue = 2500;
-		wait(10000);
+		//servo1_targetValue = 180;
+		//servo2_targetValue = 0;
+		//wait(10000);
+		//servo1_targetValue = 0;
+		//servo2_targetValue = 180;
+		//wait(10000);
+		
+		servo1_targetValue = 90;
+		servo2_targetValue = 90;
 	}
 	
 	return 1;
--- Eindopdracht/Eindopdracht/servo.c
+++ Eindopdracht/Eindopdracht/servo.c
@@ -5,12 +5,12 @@
 #include <avr/interrupt.h>
 #include "servo.h"
 
-// Maps the value between two numbers: https://stackoverflow.com/questions/345187/math-mapping-numbers
-//#define MAP(x) (x - 0) / (180 - 0) * (1500 - 250) + 250
-
+// Maps the value
+double map(double, int, int);
 
 // Start value of the servo
-static const int startValue = 0;
+static const int startValue1 = 250;
+static const int startValue2 = 250;
 
 // Extern variables for the servo target values
 int servo1_targetValue;
@@ -21,14 +21,14 @@ ISR(TIMER1_COMPA_vect)
 {
 	// Go slow to target value
 	const float speed = 1;
-	static float currentValue = startValue;
+	static float currentValue = startValue1;
 	
 	// Maps the value from 0-180 to 250-1500 (frequency of the servo)
-	//float targetValue = MAP(servo_targetValue);
+	float targetValue = map(servo1_targetValue, 250, 1300);
 	
-	if (currentValue < servo1_targetValue)
+	if (currentValue < targetValue)
 	currentValue += speed;
-	else if (currentValue > servo1_targetValue)
+	else if (currentValue > targetValue)
 	currentValue -= speed;
 	
 	OCR1A = currentValue;
@@ -38,14 +38,14 @@ ISR(TIMER1_COMPB_vect)
 {
 	// Go slow to target value
 	const float speed = 1;
-	static float currentValue = startValue;
+	static float currentValue = startValue2;
 	
-	// Maps the value from 0-180 to 250-1500 (frequency of the servo)
-	//float targetValue = MAP(servo_targetValue);
+	// Maps the value from 0-180 to 250-1300 (frequency of the servo)
+	float targetValue = map(servo2_targetValue, 250, 1300);
 	
-	if (currentValue < servo2_targetValue)
+	if (currentValue < targetValue)
 	currentValue += speed;
-	else if (currentValue > servo2_targetValue)
+	else if (currentValue > targetValue)
 	currentValue -= speed;
 	
 	OCR1B = currentValue;
@@ -53,9 +53,11 @@ ISR(TIMER1_COMPB_vect)
 
 void servo_init()
 {
-	OCR1A = 250;				// Sets the compare value of timer 1, (this is linked to port B5)
-	OCR1B = 250;				// Sets the compare value of timer 1, (this is linked to port B6)
-	servo2_targetValue = startValue;
+	OCR1A = startValue1;		// Sets the compare value of timer 1, (this is linked to port B5)
+	OCR1B = startValue2;		// Sets the compare value of timer 1, (this is linked to port B6)
+	
+	servo1_targetValue = startValue1;
+	servo2_targetValue = startValue2;
 	
 	TIMSK |= 1<<(3);			// Turn on compare mode for channel A
 	TIMSK |= 1<<(4);			// Turn on compare mode for channel B
@@ -64,4 +66,10 @@ void servo_init()
 	ICR1 = 10000;				// Sets the upper limit to timer 1
 	TCCR1A = 0b10100010;		// Set OC1B as output
 	TCCR1B = 0b00010010;		// fast PWM 8 bit, prescaler=8, RUN
+}
+
+double map(double value, int newMin, int newMax)
+{
+	// Maps the value between two numbers: https://stackoverflow.com/questions/345187/math-mapping-numbers
+	return (value - 0) / (180 - 0) * (newMax - newMin) + newMin;
 }
