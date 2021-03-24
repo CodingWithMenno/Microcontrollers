25713a5ea05005da43f92128b06429b2debf7dc9
Menno <my.bil@student.avans.nl>
2021-03-24 10:56:51 +0100

    [FEATURE] servo controlling via interrupt

--- Eindopdracht/Eindopdracht/main.c
+++ Eindopdracht/Eindopdracht/main.c
@@ -2,10 +2,15 @@
 #include <avr/io.h>
 #include <stdio.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+
+#define BIT(x) 1<<(x)
 
 void wait(int ms);
 void timer1Init();
 void setPwmTargetValue(int);
+
+static int servoValue;
 
 // Main program: Counting on T1
 int main( void )
@@ -14,17 +19,42 @@ int main( void )
 	timer1Init();
 	wait(1000);
 	
-	for (int i = 250; i < 2500; i++)
+	while (1)
 	{
-		setPwmTargetValue(i);
-		wait(1);
+		for (int i = 250; i < 2500; i++)
+		{
+			servoValue = i;
+			wait(1);
+		}
+		
+		for (int i = 2500; i > 250; i--)
+		{
+			servoValue = i;
+			wait(1);
+		}
 	}
+	
+	return 1;
+}
 
+ISR(TIMER1_COMPA_vect)
+{
+	setPwmTargetValue(servoValue);
 }
 
 void timer1Init()
 {
-	OCR1A = 250;				// Sets the compare value of timer 1
+	int startValue = 250;
+	
+	OCR1A = startValue;			// Sets the compare value of timer 1, (this is linked to port B5)
+	servoValue = startValue;
+	
+	//Zet de compare modus aan
+	TIMSK |= BIT(4);
+		
+	//Zet het interrupt systeem aan
+	sei();
+	
 	ICR1 = 10000;				// Sets the upper limit to timer 1
 	TCCR1A = 0b10000010;		// Set OC1A as output
 	TCCR1B = 0b00010010;		// fast PWM 8 bit, prescaler=8, RUN
