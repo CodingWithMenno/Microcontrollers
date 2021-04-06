d0223e74f9f09d96b8196bbf3061ac7a6f65a699
Menno <my.bil@student.avans.nl>
2021-04-06 12:05:06 +0200

    [FEATURE] camera settings implemented

--- Eindopdracht/Eindopdracht/main.c
+++ Eindopdracht/Eindopdracht/main.c
@@ -1,19 +1,45 @@
 #define F_CPU 8e6
 #include <stdio.h>
+#include <math.h>
 #include <avr/io.h>
 #include <stdlib.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include "servo.h"
 #include "lrf.h"
+#include "lcd.h"
 
 #define BIT(x) 1<<(x)
 
 void wait(int ms);
 
-// Extern variable to the servo1 angle (defined in servo.h)
+
+struct CAMERA_SETTING
+{
+	int distance; // Lrf distance
+	int focus; // Servo 1
+	int zoom; // Servo 2
+};
+
+/*
+	Servo-Distance table:
+	150 distance =  105 servo 1, 180 servo 2
+	675 distance = 44 servo 1, 80 servo 2
+	1200 distance = 40 servo 1, 30 servo 2
+*/
+
+static int totalCameraSegments = 3;
+static struct CAMERA_SETTING cameraSettings[] = 
+{
+	{ 150, 105, 180 },
+	{ 675, 44, 80 },
+	{ 1200, 40, 30 }
+};
+
+
+// Extern variable to the servo1 angle (angle between 0 and 110 (safe angles))
 int servo1_targetValue;
-// Extern variable to the servo2 angle (defined in servo.h)
+// Extern variable to the servo2 angle (angle between 180 and 30 (safe angle))
 int servo2_targetValue;
 
 
@@ -21,12 +47,12 @@ int main( void )
 {
 	DDRB = 0xFF; // Set whole port B as output
 	
-	servo_init();
-	wait(200); // Wait for the init of the servo's
-	servo1_targetValue = 90;
-	servo2_targetValue = 90;
+	init(); // Lcd init
 	
-	USART_Init(MYUBRR);
+	servo_init();
+	wait(1000); // Wait for the init of the servo's
+	
+	USART_Init(MYUBRR);	// Init the lrf
 	char *data = (char*) malloc(sizeof(char) * 20);
 	int distance = 0;
 	
@@ -35,10 +61,34 @@ int main( void )
 		USART_Transmit(SINGLE_RANGE_COMMAND_A);	// Send measure command to lrf
 		USART_Receive_String(data);	// Receive string form lrf
 		distance = USART_onlyNumbers(data);	// Get the distance from the string
+		if (distance == 0)
+		{
+			wait(2000);
+			continue;
+		}
 		
+		sprintf(data, "Distance: %d", distance);
+		clearDisplay();
+		display_text(data);	// Show the distance on the lcd
 		
+		// Get the closest zoom and focus setting from the measured distance
+		int minDifference = 1000;
+		struct CAMERA_SETTING settingToUse = cameraSettings[0];
+		for (int i = 0; i < totalCameraSegments; i++)
+		{
+			int difference = abs(distance - cameraSettings[i].distance);
+			
+			if (difference < minDifference)
+			{
+				minDifference = difference;
+				settingToUse = cameraSettings[i];
+			}
+		}
 		
-		wait(3000);			
+		servo1_targetValue = settingToUse.focus; // Focus on target
+		servo2_targetValue = settingToUse.zoom;	// Zoom to target
+		
+		wait(3000);	
 	}
 	
 	free(data);
